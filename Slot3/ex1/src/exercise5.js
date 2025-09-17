52d3cfb25f5064d39ca3d83a91a29ca95d113be1
vophammy24 <vophammy@gmail.com>
2025-09-17 08:57:11 +0700

    Slot3

---
+++ Slot3/ex1/src/exercise5.js
@@ -0,0 +1,11 @@
+var people = [
+  {name: 'Jack', age: 50},
+  {name: 'Michael', age: 9}, 
+  {name: 'John', age: 40}, 
+  {name: 'Ann', age: 19}, 
+  {name: 'Elisabeth', age: 16}
+]
+var teens = people
+  .filter(person => person.age >= 13 && person.age <= 19)
+  .map(person => `${person.name} (${person.age})`);
+teens.forEach(string => console.log(string));
