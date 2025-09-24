b74d29b9e4d6b627af1479dce22eb8e17a374525
vophammy24 <vophammy@gmail.com>
2025-09-24 09:20:23 +0700

    add Slot5

---
+++ Slot5/ex1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Exercise1 } from './Components/exercise1';
+import { Exercise2 } from './Components/exercise2';
+import { Exercise3 } from './Components/exercise3';
+
+function App() {
+  return (
+    <div>
+      <Exercise1 />
+      <Exercise2 />
+      <Exercise3 />
+    </div>
+  );
+}
+
+export default App;
