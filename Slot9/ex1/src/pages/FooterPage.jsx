9e2f28065ee083fd807f911682acc30ce11c3360
vophammy24 <vophammy@gmail.com>
2025-10-10 01:03:10 +0700

    Update LAB3

--- Slot9/ex1/src/pages/FooterPage.jsx
+++ Slot9/ex1/src/pages/FooterPage.jsx
@@ -3,7 +3,6 @@ import MyFooter from "../components/Footer/MyFooter.jsx";
 export default function FooterPage() {
     return (
     <div className="footer">
-        <h2 style={{textAlign: "center", maxWidth: 600, margin: "0 auto"}}></h2>
         <MyFooter author="Võ Phạm Mỹ" email="vophammy@gmail.com" linkGithub="Movie Management Project" />
     </div>
     )
